73b1d8322edf93a7ac6fdb5a51ec6eb7bc3f6cbb
0xilham <ilhamhermawan@hotmail.com>
2023-10-18 02:45:23 +0700

    Memperbaiki bug deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  networks: {
+    polygon: {
+      url: "http://localhost:8545", // Ganti dengan URL RPC testnet Polygon Mumbai
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"], // Ganti dengan kunci pribadi akun yang akan digunakan
+    },
+  },
+  solidity: {
+    version: "0.8.20", // Sesuaikan versi Solidity sesuai dengan kontrak Anda
+  },
 };
